0307c69c133a872a244303766e42211a6326f987
Kaushik Narayan <narayankaushik49@gmail.com>
2022-11-09 17:59:21 +0530

    Changes

--- Astar_nyc.py
+++ Astar_nyc.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 start = np.array([672,  348])
 goal = np.array([298 , 450])
@@ -9,7 +8,7 @@ grid = np.load('new_york.npy')
 path = np.zeros([len(grid), len(grid[0])], dtype=float)
 path -= 1000
 
-heuristic=[0,0,0,0,0,0,0,0,0]
+heuristic=[0,0,0,0,0,0,0,0]
 
 class Astar:
     def __init__(self,start,goal,grid,path):
@@ -28,12 +27,12 @@ class Astar:
         d = np.array([1, 0])
         l = np.array([0, -1])
         r = np.array([0, 1])
-        diag_r_lower = np.array([1,1])
-        diag_l_lower=np.array([1,-1])
-        diag_r_upper=np.array([-1,1])
-        diag_l_upper=np.array([-1,-1])
+        
 
-        potential_moves = [pos + u, pos + d, pos + l, pos + r, pos + diag_l_lower, pos + diag_l_upper, pos + diag_r_lower, pos + diag_r_upper]
+        potential_moves = [pos + u, pos + d, pos + l, pos + r]
+
+        # Diagonal moves
+        potential_moves += [pos + u+r, pos + u+l, pos + d+r, pos + d+l]
 
         # print("Potential moves :",potential_moves)
         # print(potential_moves)
@@ -52,23 +51,30 @@ class Astar:
             if self.grid[move[0],move[1],0] < 0.2:
                 potential_moves.remove(move)
         valid_moves = potential_moves
-        # print("Valid moves :",potential_moves,"\n")
+        print("Valid moves :",potential_moves,"\n")
+        # print(len(valid_moves))
         return valid_moves
 
     def next_move(self,valid_moves):
         
         # print(self.pos)
         for i,move in enumerate(valid_moves):
-            # heuristic.insert(i,np.sqrt((move[0]-self.pos[0])**2 + (move[1]-self.pos[1])**2))
-            heuristic[i]=(np.sqrt((move[0]-self.pos[0])**2 + (move[1]-self.pos[1])**2))
             
-        print(heuristic)
+            heuristic[i]=(np.sqrt((move[0]-self.goal[0])**2 + (move[1]-self.goal[1])**2))
+        
+        minpos=heuristic.index(min(heuristic))
+        self.explored_node.append(self.pos)
+        self.pos=valid_moves[minpos]
 
-            
-
-            
 
 obj= Astar(start,goal,grid,path)
 
-obj.valid_moves(obj.get_possible_moves(start))
-obj.next_move(obj.valid_moves(obj.get_possible_moves(start)))
+while obj.pos[0]!=goal[0] or obj.pos[1]!=goal[1]:
+    moves=obj.get_possible_moves(obj.pos)
+    valid_moves=obj.valid_moves(moves)
+    obj.next_move(valid_moves)
+    
+    
+
+# obj.valid_moves(obj.get_possible_moves(start))
+# obj.next_move(obj.valid_moves(obj.get_possible_moves(start)))
